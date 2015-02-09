f6b8e3f3849bfeb805e04ad75b64c0e913db9784
a-j-r <arogger@bluewin.ch>
2015-02-09 12:55:47 +0100

    v0.1

---
+++ CourseProject/GettingAndCleaningData/run_analysis.R
@@ -0,0 +1,56 @@
+# Analysis of UCI HAR Dataset
+# 
+# Author: a-j-r
+###############################################################################
+
+
+# Cleaning the workspace to have more memory at disposition.
+rm(list = ls())
+
+# Downloading and unzipping the input data files.
+if (!file.exists("./input")) { dir.create("./input")}
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(url, destfile="./input/UCI HAR Dataset.zip")
+unzip("./input/UCI HAR Dataset.zip", exdir="./input")
+
+# reading the features and create a filter for the mean and std variables.
+features <- read.table("input/UCI HAR Dataset/features.txt", quote="\"")
+mean_and_std_filter <- grepl("*-mean\\(\\)*", features[[2]]) | grepl("*-std\\(\\)*", features[[2]])
+mean_and_std_features <- features[[2]][mean_and_std_filter]
+
+# read the test set data (activities, subjects and measurements) and filter them with the created filter-vector.
+y_test <- read.table("input/UCI HAR Dataset/test/y_test.txt", quote="\"")
+names(y_test) <- "activity"
+
+subject_test <- read.table("input/UCI HAR Dataset/test/subject_test.txt", quote="\"")
+names(subject_test) <- "subject"
+
+X_test <- read.table("input/UCI HAR Dataset/test/X_test.txt", quote="\"")[,mean_and_std_filter]
+colnames(X_test) <- mean_and_std_features
+
+X_test <- cbind(subject_test, y_test, X_test)
+
+# read the train set data (activities, subjects and measurements) and filter them with the created filter-vector.
+y_train <- read.table("input/UCI HAR Dataset/train/y_train.txt", quote="\"")
+names(y_train) <- "activity"
+
+subject_train <- read.table("input/UCI HAR Dataset/train/subject_train.txt", quote="\"")
+names(subject_train) <- "subject"
+
+X_train <- read.table("input/UCI HAR Dataset/train/X_train.txt", quote="\"")[,mean_and_std_filter]
+colnames(X_train) <- mean_and_std_features
+
+X_train <- cbind(subject_train, y_train, X_train)
+
+X_complete <- rbind(X_train, X_test)
+
+# give the activities speaking labels.
+activity_labels <- read.table("input/UCI HAR Dataset/activity_labels.txt", quote="\"")
+X_complete$activity <- activity_labels[[2]][X_complete$activity]
+
+#aggregate the raw data for each subject and activity with their means.
+res <- aggregate(. ~ subject + activity, X_complete, mean)
+res <- res[order(res$subject,res$activity),]
+
+#write the result to "res.txt"
+write.table(res, "res.txt", row.names=FALSE)
